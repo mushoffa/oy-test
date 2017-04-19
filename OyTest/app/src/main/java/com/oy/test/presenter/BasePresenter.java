271b17b860485fe23e6bc0661d628f2298cbb60a
mushoffa <ridwan.mushoffa@gmail.com>
2017-04-20 00:22:22 +0700

    - Added base presenter - Adde base scheduler

---
+++ OyTest/app/src/main/java/com/oy/test/presenter/BasePresenter.java
@@ -0,0 +1,12 @@
+package com.oy.test.presenter;
+
+/**
+ * Created by mushoffa on 18/04/17.
+ */
+
+public interface BasePresenter {
+
+    void subscribe();
+
+    void unsubscribe();
+}
---
+++ OyTest/app/src/main/java/com/oy/test/scheduler/BaseSchedulerProvider.java
@@ -0,0 +1,24 @@
+package com.oy.test.scheduler;
+
+import android.support.annotation.NonNull;
+
+import rx.Scheduler;
+
+/**
+ * Created by mushoffa on 18/04/17.
+ */
+
+public interface BaseSchedulerProvider {
+
+    @NonNull
+    Scheduler computation();
+
+    @NonNull
+    Scheduler io();
+
+    @NonNull
+    Scheduler immediate();
+
+    @NonNull
+    Scheduler ui();
+}
