b47431ece4c29c3e6d8ebe46a1cfafb78ddc9eab
mushoffa <ridwan.mushoffa@gmail.com>
2017-04-21 00:49:22 +0700

    Add operational hour model

--- OyTest/app/src/main/java/com/oy/test/model/OperationalHour.java
+++ OyTest/app/src/main/java/com/oy/test/model/OperationalHour.java
@@ -4,6 +4,8 @@ import com.google.gson.annotations.SerializedName;
 
 import org.parceler.Parcel;
 
+import java.util.Map;
+
 /**
  * Created by mushoffa on 19/04/17.
  */
@@ -14,9 +16,36 @@ public class OperationalHour {
     @SerializedName("day_of_week")
     private String dayOfWeek;
 
-    @SerializedName("close_time")
+    @SerializedName("timings")
+    private Map<String, String> timings;
+
     private String closeTime;
 
-    @SerializedName("open_time")
     private String openTime;
+
+    public String getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public void setDayOfWeek(String dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    public Map<String, String> getTimings() {
+        return timings;
+    }
+
+    public void setTimings(Map<String, String> timings) {
+        this.timings = timings;
+        this.closeTime = timings.get("close_time");
+        this.openTime = timings.get("open_time");
+    }
+
+    public String getCloseTime() {
+        return closeTime;
+    }
+
+    public String getOpenTime() {
+        return openTime;
+    }
 }
